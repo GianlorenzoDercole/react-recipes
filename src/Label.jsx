afb7cbae4c3f0fa923072c40ea6311817db280e7
GianlorenzoDercole <johndercole01@gmail.com>
2022-12-05 16:58:30 -0800

    deleted extra code from recipe.jsx

--- src/Label.jsx
+++ src/Label.jsx
@@ -1,7 +1,6 @@
 import Ingredient from './Ingredient'
 import {useState} from 'react'
 export default function Label({ recipeLabel }) {
-//////////////////////////////////////////////////////////////////
 
     const [showIngredients, setShowIngredients] = useState(false)
     const ingredients = recipeLabel.recipe.ingredients.map((ingredient, idx) => {
@@ -12,7 +11,6 @@ export default function Label({ recipeLabel }) {
         <div>
             <div><button className='topButton'>{recipeLabel.recipe.label}</button></div>
             {/* {recipeLabel.recipe.label.length < 26 ? <h3>{recipeLabel.recipe.label}</h3> : <h3>{recipeLabel.recipe.label.slice(0, 30)} ...</h3>} */}
-            {/* <h3>{recipeLabel.recipe.label}</h3> */}
             { showIngredients ? < div className='ingredients'> <button onClick={() => setShowIngredients(!showIngredients)}>{showIngredients ? 'hide ingredients' : 'show ingredients'}</button><h2 className='labelForIngredients'>{recipeLabel.recipe.label}</h2><h3>{ingredients}</h3> </div>  : ''}
             <img src={recipeLabel.recipe.image}></img>
             <div><button onClick={() => setShowIngredients(!showIngredients)}>{showIngredients ? 'hide ingredients' : 'show ingredients'}</button></div>
--- src/Recipe.jsx
+++ src/Recipe.jsx
@@ -1,62 +1,38 @@
+
+
 import {
     useState,
     useEffect
 } from 'react'
 import Label from './Label'
 export default function Recipe() {
-//////////////////////////
-//
-//////////////////////////
+
     const [input, setInput] = useState('pie')
     const [recipes, setRecipes] = useState([])
-    const [item, setItem] = useState({
-        pic: '',
-        label: ''
-    })
-    const [showItem, setShowItem] = useState(true)
-
-
 
     useEffect(() => {
 
         fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${input}&app_id=750505cc&app_key=4d9f46de2a4199d804217a16f108cf24`)
         .then(res => res.json())
         .then(information => {
-            // console.log(information.hits[0].recipe.image)
-
-            const recipeInformation = [ information.hits[2].recipe.label  ]
-
-            setItem({...item, pic: information.hits[2].recipe.image, label: recipeInformation, pic22: information.hits[3].recipe.image, label22: information.hits[3].recipe.label})
 
             const recipeData = information.hits
             console.log(recipeData)
             setRecipes(recipeData)
 
 
-
-
         })
     }, [input])
 
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
 
-            setShowItem(true)
-            // console.log(recipes)
-            // console.log(input)
-            // console.log(item)
-            // console.log(recipes)
-            // console.log(recipes[5].recipe.label)
-        }
-
-        const recipeLabels = recipes.map((recipeLabel, idx) => {
-            return (
+    const recipeLabels = recipes.map((recipeLabel, idx) => {
+        return (
             <div>
-            <Label key={`key${idx}`} recipeLabel={recipeLabel}/>
+                <Label key={`key${idx}`} recipeLabel={recipeLabel}/>
             </div>
-            )
-        })
+        )
+    })
         console.log(recipes)
     return (
         <div>
@@ -65,51 +41,26 @@ export default function Recipe() {
 
                 <div>
 
-                    <form onSubmit={e => handleSubmit(e, input, setInput)}>
+                    <form >
                         <label htmlFor='name-input'> </label>
                         <input
                             type='text'
-                            // placeholder='name'
                             id='name-input'
                             value={input}
-                            onChange={(e) => { setInput(e.target.value)
-                            setShowItem(false)}}
+                            onChange={(e) => { setInput(e.target.value)}}
                         />
-                        {/* <button type='submit' >add</button> */}
                     </form>
                 </div>
-                <h3>search an ingredient or dish for recipes</h3>
+                <h2>search an ingredient or dish for recipes</h2>
             </div>
 
 
             <div className='recipes'>
-            {recipeLabels}
+                {recipeLabels}
             </div>
 
-
-        {/* { showItem ?
-            <img src={item.pic}></img> : ''
-
-        }
-
-        { showItem ?
-            <div>
-                {item.label}
-            </div> : ''
-        }
-
-        { showItem ?
-            <img src={item.pic22}></img> : ''
-
-        }
-        { showItem ?
-            <div>
-                {item.label22}
-            </div> : ''
-        } */}
 
         </div>
 
     )
 }
-
